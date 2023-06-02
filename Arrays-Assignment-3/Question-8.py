9f924b0fa5974db91d9ca0111b6e66040599e0fe
Jayanthbabu456 <babujayanth456@gmail.com>
2023-06-02 19:24:11 +0530

    questions added

---
+++ Arrays-Assignment-3/Question-8.py
@@ -0,0 +1,6 @@
+# Given an array of meeting time intervals where intervals[i] = [starti, endi],
+# determine if a person could attend all meetings.
+
+# Example 1:
+# Input: intervals = [[0,30],[5,10],[15,20]]
+# Output: false
